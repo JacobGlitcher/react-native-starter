0452d8fc25d05e37f11bc48f568633431c30bbcd
JacobGlitcher <hakob.yer@gmail.com>
2025-01-03 12:54:19 +0400

    added navigation functionality

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from "react-native";
+import {Text, View, Button, TouchableOpacity, StyleSheet} from "react-native";
 
 const styles = StyleSheet.create({
   text: {
@@ -6,8 +6,20 @@ const styles = StyleSheet.create({
   },
 })
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button
+        title="Go To Components Demo"
+        onPress={() => navigation.navigate("Components")}
+      />
+      <Button
+        title="Go To List Demo"
+        onPress={() => navigation.navigate("List")}
+      />
+    </View>
+  )
 }
 
 export default HomeScreen
